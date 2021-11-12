1b51be083feec8e845f1303fd7fcc7707b62ba20
anindyaarman <anindyarmn@gmail.com>
2021-11-12 21:07:33 +0700

    first commit

---
+++ preference_discovery/models.py
@@ -0,0 +1,95 @@
+from otree.api import (
+    models,
+    widgets,
+    BaseConstants,
+    BaseSubsession,
+    BaseGroup,
+    BasePlayer,
+    Currency as c,
+    currency_range,
+)
+import numpy as np
+import pandas as pd
+from random import sample
+
+author = 'Putu Sanjiwacika Wibisana'
+
+doc = """
+Adaptation of Preference Discovery by Delaney, Jacobson and Moenig (2018) for risk preference discovery.
+"""
+
+
+class Constants(BaseConstants):
+    name_in_url = 'preference_discovery_v2'
+    players_per_group = None
+    num_rounds = 20
+    with open('preference_discovery/Lottery.csv', encoding="utf-8") as file:
+        prospects = pd.read_csv(file)
+
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Group(BaseGroup):
+    pass
+
+
+class Player(BasePlayer):
+
+    def set_player_param(self):
+        # round settings
+        self.training_round = 1 if self.round_number <= self.session.config["training_rounds"] else 0
+        if self.round_number == 1:
+            self.participant.vars["prospect_table"] = Constants.prospects
+            self.endowment = self.session.config["endowment"]
+            self.participant.vars["payoff_vector"] = list()
+        elif self.round_number == self.session.config["training_rounds"] + 1:
+            self.participant.vars["prospect_table"] = Constants.prospects
+        #randomizer
+        rand = sample(list(range(0,20)),4)
+        rand.append(20)
+        self.participant.vars["random_indexes"] = rand
+        self.participant.vars["displayed_lotteries"] = list(self.participant.vars["prospect_table"].loc[self.participant.vars["random_indexes"],"Index"])
+        self.participant.vars["displayed_prospects"] = self.participant.vars["prospect_table"].loc[self.participant.vars["random_indexes"],:]
+        self.displayed_lotteries = str(list(self.participant.vars["displayed_lotteries"]))
+
+    def payoff_realizer(self):
+        df = self.participant.vars["displayed_prospects"]
+        df[["Allocation"]] = [self.Lotere_A, self.Lotere_B, self.Lotere_C, self.Lotere_D, self.Lotere_E]                ### df[["Allocation"]] = [0,0,2,1,2]
+        df[["payoff"]] = [0, 0, 0, 0, 0]
+        for i in self.participant.vars["random_indexes"]:
+            df.loc[i,"A_or_B"] = np.random.choice(["A","B"], p=[df.loc[i,"p1"],df.loc[i,"p2"]])
+            df.loc[i,"payoff"] = df.loc[i,"x1"] * df.loc[i,"Allocation"] if df.loc[i,"A_or_B"] == "A" else df.loc[i,"x2"] * df.loc[i,"Allocation"]
+        self.payoff_thisround = int(df[["payoff"]].sum())
+        if self.training_round == False:
+            self.participant.vars["payoff_vector"].append(self.payoff_thisround)
+        self.participant.vars["prospect_table"].update(df)
+        for i in range(0,len(self.participant.vars["prospect_table"])):
+            if self.participant.vars["prospect_table"].loc[i,"A_or_B"] != "X":
+                if self.participant.vars["prospect_table"].loc[i,"A_or_B"] == "A":
+                    self.participant.vars["prospect_table"].loc[i, "p1"] = 1
+                    self.participant.vars["prospect_table"].loc[i, "p2"] = 0
+                elif self.participant.vars["prospect_table"].loc[i,"A_or_B"] == "B":
+                    self.participant.vars["prospect_table"].loc[i, "p1"] = 0
+                    self.participant.vars["prospect_table"].loc[i, "p2"] = 1
+            else:
+                pass
+        self.participant.vars["displayed_prospects"] = df
+
+    endowment = models.IntegerField()
+    payoff_thisround = models.IntegerField()
+    displayed_lotteries = models.StringField()
+    training_round = models.BooleanField()
+
+    Lotere_A = models.IntegerField(min=0, max=10, initial=0)
+    Lotere_B = models.IntegerField(min=0, max=10, initial=0)
+    Lotere_C = models.IntegerField(min=0, max=10, initial=0)
+    Lotere_D = models.IntegerField(min=0, max=10, initial=0)
+    Lotere_E = models.IntegerField(min=0, max=10, initial=0)
+
+    ## Vars for questionnaire
+
+    Name = models.StringField(label="Nama Lengkap Anda:")
+    Age = models.IntegerField(label="Usia:", min=14, max=35)
+    Gender = models.StringField(label="Gender:", choices=["Pria", "Wanita"])
---
+++ preference_discovery/pages.py
@@ -0,0 +1,129 @@
+from otree.api import Currency as c, currency_range
+from ._builtin import Page, WaitPage
+from .models import Constants
+import ast
+import random
+import numpy as np
+import math
+
+
+class No1Introduction(Page):
+
+    def is_displayed(self):
+        return self.round_number == 1
+
+
+class No2Instructions1(Page):
+
+    def is_displayed(self):
+        return self.round_number == 1
+
+    def vars_for_template(self):
+        return {
+            'endowment': self.session.config["endowment"],
+            'instrument' : "preference_discovery/SC1.jpg",
+            'result': "preference_discovery/SC2.jpg",
+            'show_up_fee': int(self.session.config["participation_fee"]),
+        }
+
+class No2Warning(Page):
+
+    def is_displayed(self):
+        return self.round_number == self.session.config["training_rounds"] + 1
+
+
+class No3Start(Page):
+
+    def is_displayed(self):
+        return self.round_number <= self.session.config['rounds']
+
+    def before_next_page(self):
+        return {self.player.set_player_param()}
+
+    def vars_for_template(self):
+        return {
+            'training': self.round_number <= self.session.config["training_rounds"],
+            'training_round': self.round_number,
+            'round': self.round_number - self.session.config["training_rounds"],
+        }
+
+
+class No4Purchase(Page):
+
+    def is_displayed(self):
+        return self.round_number <= self.session.config['rounds']
+
+    def vars_for_template(self):
+        p = self.participant.vars["displayed_prospects"]
+        return {
+            'p': self.participant.vars["displayed_prospects"],
+            'rand_index': self.participant.vars["random_indexes"],
+            'payoff_vector': self.participant.vars["payoff_vector"],
+            'endowment': self.session.config["endowment"],
+            'training': self.round_number <= self.session.config["training_rounds"],
+            'training_round': self.round_number,
+            'round': self.round_number - self.session.config["training_rounds"],
+            'lot_1': p.iloc[0,2],'gain_A_1': p.iloc[0,3],'prob_A_1': p.iloc[0,4],'gain_B_1': p.iloc[0,5],'prob_B_1': p.iloc[0,6],'rel_1': p.iloc[0,7],
+            'lot_2': p.iloc[1,2],'gain_A_2': p.iloc[1,3],'prob_A_2': p.iloc[1,4],'gain_B_2': p.iloc[1,5],'prob_B_2': p.iloc[1,6],'rel_2': p.iloc[1,7],
+            'lot_3': p.iloc[2,2],'gain_A_3': p.iloc[2,3],'prob_A_3': p.iloc[2,4],'gain_B_3': p.iloc[2,5],'prob_B_3': p.iloc[2,6],'rel_3': p.iloc[2,7],
+            'lot_4': p.iloc[3,2],'gain_A_4': p.iloc[3,3],'prob_A_4': p.iloc[3,4],'gain_B_4': p.iloc[3,5],'prob_B_4': p.iloc[3,6],'rel_4': p.iloc[3,7],
+            'lot_5': p.iloc[4,2],'gain_A_5': p.iloc[4,3],'prob_A_5': p.iloc[4,4],'gain_B_5': p.iloc[4,5],'prob_B_5': p.iloc[4,6],'rel_5': p.iloc[4,7],
+            'df': self.participant.vars["prospect_table"],
+            'pagehold_timer': self.session.config['submit_delay'],
+            'pagehold_timer_ths': self.session.config['submit_delay'] * 1000,
+        }
+
+    form_model = 'player'
+
+    def get_form_fields(self):
+        fields = ['Lotere_A', 'Lotere_B', 'Lotere_C', 'Lotere_D', 'Lotere_E']
+        return fields
+
+    def error_message(self, values):
+        if values['Lotere_A'] + values['Lotere_B'] + values['Lotere_C'] + values['Lotere_D'] + values['Lotere_E'] > self.session.config["endowment"]:
+            return 'Total alokasi untuk seluruh alternatif tidak boleh lebih dari ' + str(self.session.config["endowment"]) + " poin!"
+
+    def before_next_page(self):
+        return {self.player.payoff_realizer()}
+
+
+class No5Result(Page):
+
+    def is_displayed(self):
+        return self.round_number <= self.session.config['rounds']
+
+    def vars_for_template(self):
+        df = self.participant.vars["displayed_prospects"][["x1","x2","Allocation","A_or_B","payoff"]]
+        return {
+            'training': self.round_number <= self.session.config["training_rounds"],
+            'training_round': self.round_number,
+            'round': self.round_number - self.session.config["training_rounds"],
+            'A1': df.iloc[0, 0], 'B1': df.iloc[0, 1], 'C1': df.iloc[0, 2], 'D1': df.iloc[0, 3], 'E1': df.iloc[0, 4],
+            'A2': df.iloc[1, 0], 'B2': df.iloc[1, 1], 'C2': df.iloc[1, 2], 'D2': df.iloc[1, 3], 'E2': df.iloc[1, 4],
+            'A3': df.iloc[2, 0], 'B3': df.iloc[2, 1], 'C3': df.iloc[2, 2], 'D3': df.iloc[2, 3], 'E3': df.iloc[2, 4],
+            'A4': df.iloc[3, 0], 'B4': df.iloc[3, 1], 'C4': df.iloc[3, 2], 'D4': df.iloc[3, 3], 'E4': df.iloc[3, 4],
+            'A5': df.iloc[4, 0], 'B5': df.iloc[4, 1], 'C5': df.iloc[4, 2], 'D5': df.iloc[4, 3], 'E5': df.iloc[4, 4],
+            'payoff_thisround': self.player.payoff_thisround,
+        }
+
+
+class No6EndQuestionnaire(Page):
+
+    def is_displayed(self):
+        return self.round_number == self.session.config['rounds']
+
+
+class No6EndResult(Page):
+
+    def is_displayed(self):
+        return self.round_number == self.session.config['rounds']
+
+
+page_sequence = [No1Introduction,
+                 No2Instructions1,
+                 No2Warning,
+                 No3Start,
+                 No4Purchase,
+                 No5Result,
+                 No6EndQuestionnaire,
+                 No6EndResult]
